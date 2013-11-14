af0c10875f53ffb6cbbd52b4b5658e750e169cc1
Zooko Wilcox-O'Hearn <zooko@zooko.com>
2013-11-14 02:36:21 +0000

    fix one test, make a new test that is red
    Fix the first test. It is green. I edited the code-under-test to write out the
    wrong data and it made the test turn red.
    
    Added a new test which is supposed to exercise #1670. It is red. Not sure if
    that means the test is run or the code-under-test is still exhibiting #1670
    even though I ripped out retrying. (The fact is that the code does
    servermap-update more times than it ought to even *without* retrying, so it is
    entirely possible that the current code-under-tets is still harboring an
    instance of the #1670 bug and this test is demonstrating that.)

--- src/allmydata/test/test_mutable_with_mockery.py
+++ src/allmydata/test/test_mutable_with_mockery.py
@@ -18,37 +18,34 @@ class MockFileCap(uri.WriteableMDMFFileURI):
     def is_mutable(self):
         return True
 
+k = 3
+n = 10
+default_encoding_parameters = { 'k': k, 'n': n }
+
+filecontents = [ "FILE CONTENTS 1",  "FILE CONTENTS 2", "FILE CONTENTS 3",]
+
+verinfos = [
+    (0, 'mock root hash 0', 'mock IV', 99, len(filecontents[0]), k, n, 'mock prefix', ()),
+    (1, 'mock root hash 1', 'mock IV', 99, len(filecontents[1]), k, n, 'mock prefix', ()),
+    (2, 'mock root hash 2', 'mock IV', 99, len(filecontents[2]), k, n, 'mock prefix', ()),
+    ]
+
 class MutableTest(unittest.TestCase):
 
     def test_modify_existing_file(self):
         """
         If a mutable filenode is asked to modify the contents, and the
-        servers serve up shares of version 1 when initially asked, but then
-        server up shares of version 2 if asked a second time, then the
-        mutable filenode ought to use the most recent version that it learned
-        about when it is generating its new contents.
-
-        This is inspired by #1670:
-
-        https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1670
+        servers serve up shares of version 1 any time you ask, then the
+        mutable filenode ought to use version 1 when it is generating its new
+        contents.
         """
 
-        orig = "OLD FILE CONTENTS"
-
-        k = 3
-        n = 10
-        default_encoding_parameters = { 'k': k, 'n': n }
-
-        verinfo_1 = (1, 'mock root hash 1', 'mock IV', 99, len(orig), k, n, 'mock prefix', ())
-        verinfo_2 = (2, 'mock root hash 2', 'mock IV', 99, len(orig), k, n, 'mock prefix', ())
-        verinfo_3 = (3, 'mock root hash 3', 'mock IV', 99, len(orig), k, n, 'mock prefix', ())
-
         sM = mock.Mock()
-        sM.best_recoverable_version.return_value = verinfo_1
+        sM.best_recoverable_version.return_value = verinfos[0]
         sM.get_last_update.return_value = (MODE_WRITE,)
-        sM.recoverable_versions.return_value = (verinfo_1,)
-        sM.make_versionmap.return_value = {verinfo_1: "SOMETHING"}
-        sM.highest_seqnum.return_value = verinfo_1[0]
+        sM.recoverable_versions.return_value = (verinfos[0],)
+        sM.make_versionmap.return_value = {verinfos[0]: "SOMETHING"}
+        sM.highest_seqnum.return_value = verinfos[0][0]
 
         mock_sM_class = mock.Mock()
         mock_sM_class.return_value = sM
@@ -68,7 +65,7 @@ class MutableTest(unittest.TestCase):
         self.patch(allmydata.mutable.filenode, 'ServermapUpdater', mock_smU_class)
 
         mockDownloadResults = mock.Mock()
-        mockDownloadResults.chunks = [orig,]
+        mockDownloadResults.chunks = [filecontents[0],]
 
         mockRetrieveObj = mock.Mock()
         def mock_download(*args, **kwargs):
@@ -80,9 +77,16 @@ class MutableTest(unittest.TestCase):
         mockRetrieveClass.return_value = mockRetrieveObj
         self.patch(allmydata.mutable.filenode, 'Retrieve', mockRetrieveClass)
 
+        saved_mutable_data = [None]
         mockPublishObj = mock.Mock()
-        #XXXmockPublishObj.publish.return_value = defer.succeed(mockPublishResults)
-        #XXXmockPublishResults = mock.Mock()
+        def mock_publish(mutabledata):
+            saved_mutable_data[0] = mutabledata
+            d = defer.Deferred()
+            def _ev():
+                d.callback(None)
+            eventually(_ev)
+            return d
+        mockPublishObj.publish.side_effect = mock_publish
 
         mockPublishClass = mock.Mock()
         mockPublishClass.return_value = mockPublishObj
@@ -99,29 +103,142 @@ class MutableTest(unittest.TestCase):
         filenobj = filenode.MutableFileNode(mstorage_broker, msecret_holder, default_encoding_parameters, mhistory)
         filenobj.init_from_cap(mfilecap)
         
-        # XXX make it so if you download v1 you get "VERSION 1 STUFF" and if
-        # you download v2 you get "VERSION 2 STUFF".
-
         def modifier(old):
             return old + "...NEW STUFF"
 
         d = filenobj.modify(modifier)
         def _then(ign):
-            return filenobj.download_best_version()
+            # Okay at the point the filenobj can get a green flag if it has
+            # pushed the right new-version to the Publish object.
+            smd = saved_mutable_data[0]
+            pos = smd._filehandle.tell()
+            smd._filehandle.seek(0)
+            data = smd._filehandle.read()
+            smd._filehandle.seek(pos)
+
+            self.failUnlessEqual(data, filecontents[0] + "...NEW STUFF")
         d.addCallback(_then)
                           
-        def _then2(result):
-            self.failUnlessEqual(result, orig + "...NEW STUFF")
-        d.addCallback(_then2)
-
-        def _dump_calls(whatever):
-            print "xxx sMc got this: ", mock_sM_class.call_args
-            print "xxx sM got this: ", sM.method_calls
-            print "xxx smU got this: ", smU.call_args
-            print "xxx mockRetrieveObj got this: ", mockRetrieveObj.method_calls
-            print "xxx mockPublishObj got this: ", mockPublishObj.method_calls
-            return whatever
-
-        d.addBoth(_dump_calls)
         return d
-    test_modify_existing_file.timeout = 2
+
+    def test_write_collision(self):
+        """
+        If a mutable filenode is asked to modify the contents, and the
+        servers serve up shares of version 1 when initially asked, but then
+        server up shares of version 2 if asked a second time, then the
+        mutable filenode ought to stop and report a UCWE and not do anything
+        stupid like, say, blow away version 2, or get an internal KeyError.
+
+        This is inspired by #1670:
+
+        https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1670
+        """
+        class MockServerMap(object):
+            def __init__(self, *args, **kwargs):
+                self.how_many_times_asked = 0
+
+            def best_recoverable_version(self):
+                verinfo = verinfos[self.how_many_times_asked]
+                self.how_many_times_asked += 1
+                return verinfo
+
+            def get_last_update(self):
+                return (MODE_WRITE,)
+
+            def recoverable_versions(self):
+                return (verinfos[self.how_many_times_asked],)
+
+            def make_versionmap(self):
+                return {verinfos[self.how_many_times_asked]: "SOMETHING"}
+
+            def highest_seqnum(self):
+                return verinfos[self.how_many_times_asked][0]
+
+        sM = MockServerMap()
+
+        mock_sM_class = mock.Mock()
+        mock_sM_class.return_value = sM
+
+        smU = mock.Mock()
+        def mock_smU_class(filenode=filenode, *args, **kwargs):
+            def mock_update(*args, **kwargs):
+                # side effect of this is to set the pubkey
+                filenode._populate_pubkey('DUMMY PUB KEY')
+                filenode._populate_privkey('DUMMY PRIV KEY')
+                return defer.succeed(sM)
+            smU.update.side_effect = mock_update
+            return smU
+
+        import allmydata.mutable.filenode
+        self.patch(allmydata.mutable.filenode, 'ServerMap', mock_sM_class)
+        self.patch(allmydata.mutable.filenode, 'ServermapUpdater', mock_smU_class)
+
+        mockDownloadResults = mock.Mock()
+        mockDownloadResults.chunks = [filecontents[0],]
+
+        mockRetrieveObj = mock.Mock()
+        def mock_download(*args, **kwargs):
+            return defer.succeed(mockDownloadResults)
+
+        mockRetrieveObj.download.side_effect = mock_download
+
+        mockRetrieveClass = mock.Mock()
+        mockRetrieveClass.return_value = mockRetrieveObj
+        self.patch(allmydata.mutable.filenode, 'Retrieve', mockRetrieveClass)
+
+        saved_mutable_data = [None]
+        mockPublishObj = mock.Mock()
+        def mock_publish(mutabledata):
+            saved_mutable_data[0] = mutabledata
+            d = defer.Deferred()
+            def _ev():
+                d.callback(None)
+            eventually(_ev)
+            return d
+        mockPublishObj.publish.side_effect = mock_publish
+
+        mockPublishClass = mock.Mock()
+        mockPublishClass.return_value = mockPublishObj
+        self.patch(allmydata.mutable.filenode, 'Publish', mockPublishClass)
+
+        mstorage_broker = mock.Mock()
+
+        msecret_holder = mock.Mock(name='msecret_holder')
+        mhistory = mock.Mock()
+        mnodemaker = mock.Mock(name='mnodemaker')
+        muploader = mock.Mock(name='muploader')
+        mfilecap = MockFileCap()
+
+        filenobj = filenode.MutableFileNode(mstorage_broker, msecret_holder, default_encoding_parameters, mhistory)
+        filenobj.init_from_cap(mfilecap)
+        
+        def modifier(old):
+            return old + "...NEW STUFF"
+
+        d = filenobj.modify(modifier)
+        def _then(ign):
+            print "XXX sM how many times ", sM.how_many_times_asked
+
+            # Okay at the point the filenobj gets a red flag if it has pushed
+            # a version-1-based file, because that will have stomped on
+            # someone else's version 2!
+            smd = saved_mutable_data[0]
+            pos = smd._filehandle.tell()
+            smd._filehandle.seek(0)
+            data = smd._filehandle.read()
+            smd._filehandle.seek(pos)
+
+            self.failIfEqual(data, filecontents[0] + "...NEW STUFF")
+        d.addCallback(_then)
+        return d
+
+        #XXXdef _dump_calls(whatever):
+        #XXX    print "xxx sMc got this: ", mock_sM_class.call_args
+        #XXX    print "xxx sM got this: ", sM.method_calls
+        #XXX    print "xxx smU got this: ", smU.call_args
+        #XXX    print "xxx mockRetrieveObj got this: ", mockRetrieveObj.method_calls
+        #XXX    print "xxx mockPublishObj got this: ", mockPublishObj.method_calls
+        #XXX    
+        #XXX    print "xxx mockPublishObj's saved_mutable_data: ", 
+        #XXX    return whatever
+        #XXXd.addBoth(_dump_calls)
