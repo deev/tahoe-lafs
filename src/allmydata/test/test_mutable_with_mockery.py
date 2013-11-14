00c4d2dcc298675afefd70e676a306fec8dcf9f6
Zooko Wilcox-O'Hearn <zooko@zooko.com>
2013-11-14 16:12:47 +0000

    incomplete test added: don't ask over and over
    Don't ask for servermap updates over and over, that is.

--- src/allmydata/test/test_mutable_with_mockery.py
+++ src/allmydata/test/test_mutable_with_mockery.py
@@ -242,3 +242,115 @@ class MutableTest(unittest.TestCase):
         #XXX    print "xxx mockPublishObj's saved_mutable_data: ", 
         #XXX    return whatever
         #XXXd.addBoth(_dump_calls)
+
+    def test_dont_repeat_your_question(self):
+        """
+        If a mutable filenode is asked to modify the contents, then it should
+        not ask for a servermap more than once. Just ask once and then work
+        with what you get.
+
+        This is inspired by #1670:
+
+        https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1670
+
+        This test was suggested by Daira.
+        """
+        class MockServerMap(object):
+            def __init__(self, *args, **kwargs):
+                self.how_many_times_asked = 0
+
+            def best_recoverable_version(self):
+                self.failUnlessEqual(self.how_many_times_asked, 0)
+                verinfo = verinfos[self.how_many_times_asked]
+                self.how_many_times_asked += 1
+                return verinfo
+
+            def get_last_update(self):
+                return (MODE_WRITE,)
+
+            def recoverable_versions(self):
+                return (verinfos[self.how_many_times_asked],)
+
+            def make_versionmap(self):
+                return {verinfos[self.how_many_times_asked]: "SOMETHING"}
+
+            def highest_seqnum(self):
+                return verinfos[self.how_many_times_asked][0]
+
+        sM = MockServerMap()
+
+        mock_sM_class = mock.Mock()
+        mock_sM_class.return_value = sM
+
+        smU = mock.Mock()
+        def mock_smU_class(filenode=filenode, *args, **kwargs):
+            def mock_update(*args, **kwargs):
+                # side effect of this is to set the pubkey
+                filenode._populate_pubkey('DUMMY PUB KEY')
+                filenode._populate_privkey('DUMMY PRIV KEY')
+                return defer.succeed(sM)
+            smU.update.side_effect = mock_update
+            return smU
+
+        import allmydata.mutable.filenode
+        self.patch(allmydata.mutable.filenode, 'ServerMap', mock_sM_class)
+        self.patch(allmydata.mutable.filenode, 'ServermapUpdater', mock_smU_class)
+
+        mockDownloadResults = mock.Mock()
+        mockDownloadResults.chunks = [filecontents[0],]
+
+        mockRetrieveObj = mock.Mock()
+        def mock_download(*args, **kwargs):
+            return defer.succeed(mockDownloadResults)
+
+        mockRetrieveObj.download.side_effect = mock_download
+
+        mockRetrieveClass = mock.Mock()
+        mockRetrieveClass.return_value = mockRetrieveObj
+        self.patch(allmydata.mutable.filenode, 'Retrieve', mockRetrieveClass)
+
+        saved_mutable_data = [None]
+        mockPublishObj = mock.Mock()
+        def mock_publish(mutabledata):
+            saved_mutable_data[0] = mutabledata
+            d = defer.Deferred()
+            def _ev():
+                d.callback(None)
+            eventually(_ev)
+            return d
+        mockPublishObj.publish.side_effect = mock_publish
+
+        mockPublishClass = mock.Mock()
+        mockPublishClass.return_value = mockPublishObj
+        self.patch(allmydata.mutable.filenode, 'Publish', mockPublishClass)
+
+        mstorage_broker = mock.Mock()
+
+        msecret_holder = mock.Mock(name='msecret_holder')
+        mhistory = mock.Mock()
+        mnodemaker = mock.Mock(name='mnodemaker')
+        muploader = mock.Mock(name='muploader')
+        mfilecap = MockFileCap()
+
+        filenobj = filenode.MutableFileNode(mstorage_broker, msecret_holder, default_encoding_parameters, mhistory)
+        filenobj.init_from_cap(mfilecap)
+        
+        def modifier(old):
+            return old + "...NEW STUFF"
+
+        d = filenobj.modify(modifier)
+        def _then(ign):
+            print "XXX sM how many times ", sM.how_many_times_asked
+
+            # Okay at the point the filenobj gets a red flag if it has pushed
+            # a version-1-based file, because that will have stomped on
+            # someone else's version 2!
+            smd = saved_mutable_data[0]
+            pos = smd._filehandle.tell()
+            smd._filehandle.seek(0)
+            data = smd._filehandle.read()
+            smd._filehandle.seek(pos)
+
+            self.failIfEqual(data, filecontents[0] + "...NEW STUFF")
+        d.addCallback(_then)
+        return d
